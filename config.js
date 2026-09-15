f29a15dc6f5ba3498547566c2f6fcfdeadee0365
lutfianna874-cpu <lutfianna874@gmail.com>
2026-09-15 07:06:41 -0700

    add admin dashboard and database
    add admin dashboard and database

---
+++ config.js
@@ -0,0 +1,6 @@
+/* =====================================================
+   KONFIGURASI DATABASE WEBSITE DESA
+   Ganti WEB_APP_URL setelah Google Apps Script selesai
+   di-deploy sebagai Web App.
+===================================================== */
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzN1j6Bhyy8_RZTvsK-aPzVNxKlG0JPtqLNzhJueziXDvt0isxag3VrTkiwtzxn1fU/exec";
