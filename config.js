9835f4e348bc5784cf8b2a884813c0f3e196e74a
lutfianna874-cpu <lutfianna874@gmail.com>
2026-09-17 01:46:10 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* =====================================================
    KONFIGURASI DATABASE WEBSITE DESA
-   Ganti WEB_APP_URL setelah Google Apps Script selesai
-   di-deploy sebagai Web App.
+   Google Apps Script - Pengaduan Masyarakat
 ===================================================== */
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzN1j6Bhyy8_RZTvsK-aPzVNxKlG0JPtqLNzhJueziXDvt0isxag3VrTkiwtzxn1fU/exec";
+
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyLNaQxT-RRYPipTV3ohlfHQ07TIcx42VCc7pz2cKAGb8kUFWp8wXwwFO8YUnLnGr8G/exec";
